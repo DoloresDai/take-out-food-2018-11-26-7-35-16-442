a915e56a2ed862b810911cd1b3cc3639a09fb86b
DoloresDai <841856753@qq.com>
2018-12-21 11:45:28 +0800

    Modify code

--- src/best-charge.js
+++ src/best-charge.js
@@ -3,30 +3,31 @@
 //2.compare
 //3.output
 function bestCharge(selectedItems) {
-  var originalPrice = 0;
-  var specialPrice = 0;
-  var halfPrice = 0;
-  var temp = 0;
-  var allItems = loadAllItems;
-  var allPromotions = loadPromotions;
+  var originalPrice = 0,
+    specialPrice = 0,
+    halfPrice = 0,
+    temp = 0;
+  var allItems = loadAllItems,
+    allPromotions = loadPromotions;
+  var numbers = [],
+    selectedNames = [];
   for (let i = 0; i < selectedItems.length; i++) {
-    let items = selectedItems[i].split(" x ")[0];
-    let numbers = selectedItems[i].split(" x ")[1];
-    for (let j = 0; j < allItems.length; j++) {
-      if (allItems[j].id = items && items != 'ITEM0001' || 'ITEM0022') {
-        originalPrice += allItems[j].price * numbers;
-      } else if (items == 'ITEM0001' || 'ITEM0022') {
-        halfPrice += allItems[j].price / 2 * numbers;
-      }
-    }
-    temp = originalPrice;
-    originalPrice += halfPrice * 2;
-    halfPrice += temp;
-    specialPrice = originalPrice - Math.floor(originalPrice % 30) * 6;
-    if (halfPrice > specialPrice) {
-      return specialPrice;
-    } else {
-      return halfPrice;
+    let selectedItemsObj = allItems.filter(function(elements, index, allItems) {
+      allItems[index].id == selectedItems[i].split(" x ")[0];
+    });
+    numbers.push(selectedItems[i].split(" x ")[1]);
+    items.push(selectedItems[i].split(" x ")[0]);
+    unitNumbers = selectedItems[i].split(" x ")[1];
+  }
+  for (let j = 0; j < allItems.length; j++) {
+    if (allItems[j].id = items && items != 'ITEM0001' && items != 'ITEM0022') {
+      originalPrice += allItems[j].price * unitNumbers;
+    } else if (items == 'ITEM0001' || items = 'ITEM0022') {
+      halfPrice += allItems[j].price / 2 * unitNumbers;
     }
   }
+  temp = originalPrice;
+  originalPrice += halfPrice * 2;
+  halfPrice += temp;
+  specialPrice = originalPrice - Math.floor(originalPrice % 30) * 6;
 }
