ce72259c181c6b4aa30ebee7db6de8436efd1cce
DoloresDai <841856753@qq.com>
2018-12-25 14:42:09 +0800

    Use ES6 to rebulid function

--- src/best-charge.js
+++ src/best-charge.js
@@ -1,41 +1,45 @@
 //tasking
-//1.get price and count
-//2.compare
-//3.output
-function bestCharge(selectedItems) {
-  var originalPrice = 0,
-    specialPrice = 0,
-    halfPrice = 0,
-    temp = 0;
-  var allItems = loadAllItems,
-    allPromotions = loadPromotions;
-  var numbers = [],
-    selectedNames = [];
-  for (let i = 0; i < selectedItems.length; i++) {
-    let selectedItemsObj = allItems.filter(function(elements, index, allItems) {
-      allItems[index].id == selectedItems[i].split(" x ")[0];
-    });
-    numbers.push(selectedItems[i].split(" x ")[1]);
-    items.push(selectedItems[i].split(" x ")[0]);
-    unitNumbers = selectedItems[i].split(" x ")[1];
-  }
-  for (let j = 0; j < allItems.length; j++) {
-    if (allItems[j].id = items && items != 'ITEM0001' && items != 'ITEM0022') {
-      originalPrice += allItems[j].price * unitNumbers;
-    } else if (items == 'ITEM0001' || items = 'ITEM0022') {
-      halfPrice += allItems[j].price / 2 * unitNumbers;
-    }
-  }
-  temp = originalPrice;
-  originalPrice += halfPrice * 2;
-  halfPrice += temp;
-  specialPrice = originalPrice - Math.floor(originalPrice % 30) * 6;
+//1.get selected items'array,including id/name/price,just like[{id/name/price},{id/name/price}];
+//2.get special items'array and common items'array,just like[{id/name/price},{id/name/price}];
+//3.use countPrice function to get several price;
+//4.orderItemsList/useDiscount/totalPrice;
+//5.output
+function getSelectedItems(selectedItems) {
+  let selectedItemsId = [];
+  selectedItemsId.push(selectedItems[i].split(" x ")[0]);
+  var selectedItemsArray = [];
+  selectedItemsArray.push(allItems.filter(x => selectedItemsId.includes(x.id)));
+  return selectedItemsArray;
 }
 
-function orderItems(selectedItemsObj) {
+function getItems(specialIdArray) {
+  return selectedItemsArray.filter(x => specialIdArray.includes(x.id));
+}
+
+function countPrice(arrays) {
+  return arrays.reduce((x, y) => x.price + y.price);
+}
+
+function bestCharge(selectedItems) {
+  var numbers = [];
+  var originalPrice = 0,
+    specialPrice = 0,
+    halfPrice = 0;
+  numbers.push(selectedItems[i].split(" x ")[1]);
+  var halfItems = getItems(['ITEM0001', 'ITEM0022']);
+  var otherItems = getItems(['ITEM0013', 'ITEM0030']);
+  halfPrice = countPrice(halfItems) / 2 + countPrice(otherItems);
+  originalPrice = countPrice(selectedItemsArray);
+  specialPrice = originalPrice - Math.floor(originalPrice % 30) * 6;
+  orderItemsList;
+  useDiscount;
+  totalPrice;
+}
+
+function orderItemsList() {
   document.write(`============= 订餐明细 =============`);
   for (let n = 0; n < numbers.length; n++) {
-    document.write(`${selectedItemsObj[n].name} x ${numbers[n]} = ${selectedItemsObj[n].price*numbers[n]}元
+    document.write(`${selectedItemsArray[n].name} x ${number[n]} = ${selectedItemsArray[n].price*numbers[n]}元
       -----------------------------------`);
   }
 
@@ -43,7 +47,7 @@ function orderItems(selectedItemsObj) {
     document.wirte(`-----------------------------------
     使用优惠：`)
     for (let k = 0; k < numbers.length; k++) {
-      specialPrice < halfPrice ? document.wirte(`满30减6元，省${originalPrice- specialPrice}元`) : document.wirte(`使用优惠: 指定菜品半价(${selectedItems[k].name})， 省${selectedItems[k].price*numbers[k]/2}元`);
+      specialPrice < halfPrice ? document.wirte(`满30减6元，省${originalPrice- specialPrice}元`) : document.wirte(`使用优惠: 指定菜品半价(${halfItems[k].name})， 省${originalPrice- halfPrice}元`);
     }
   }
 
